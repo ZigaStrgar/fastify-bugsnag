639d6f2b880e377871ede38b180a2999dd3d9ddc
ZigaStrgar <ziga.strgar@gmail.com>
2021-05-14 22:09:32 +0200

    Standard formatting

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
-const Bugsnag = require('@bugsnag/js');
-const fp = require('fastify-plugin');
+const Bugsnag = require('@bugsnag/js')
+const fp = require('fastify-plugin')
 
 function extractRequestMetadata (request) {
-    return {
-        body: request.body,
-        clientIp: request.ips && request.ips[0] ? request.ips[0] : request.ip,
-        headers: request.raw.headers,
-        httpMethod: request.routerMethod,
-        params: request.params,
-        path: request.routerPath,
-        query: request.query,
-        referer: request.raw.headers.referer,
-        url: request.url,
-    };
+  return {
+    body: request.body,
+    clientIp: request.ips && request.ips[0] ? request.ips[0] : request.ip,
+    headers: request.raw.headers,
+    httpMethod: request.routerMethod,
+    params: request.params,
+    path: request.routerPath,
+    query: request.query,
+    referer: request.raw.headers.referer,
+    url: request.url
+  }
 }
 
 function obtainRequestAndMetadataInfo (request) {
-    const metadata = extractRequestMetadata(request);
+  const metadata = extractRequestMetadata(request)
 
-    return {
-        metadata,
-        request: {
-            clientIp: metadata.clientIp,
-            headers: metadata.headers,
-            httpMethod: metadata.httpMethod,
-            referer: metadata.referer,
-            url: metadata.url,
-        },
-    };
+  return {
+    metadata,
+    request: {
+      clientIp: metadata.clientIp,
+      headers: metadata.headers,
+      httpMethod: metadata.httpMethod,
+      referer: metadata.referer,
+      url: metadata.url
+    }
+  }
 }
 
 function bugsnagPlugin (fastify, options, done) {
-    Bugsnag.start({
-        apiKey: options.apiKey ? options.apiKey : process.env.BUGSNAG_API_KEY,
-        ...(options.bugsnagOptions || {}),
-    });
+  Bugsnag.start({
+    apiKey: options.apiKey ? options.apiKey : process.env.BUGSNAG_API_KEY,
+    ...(options.bugsnagOptions || {})
+  })
 
-    fastify.decorate('bugsnag', Bugsnag);
-    fastify.decorateRequest('bugsnag', null);
+  fastify.decorate('bugsnag', Bugsnag)
+  fastify.decorateRequest('bugsnag', null)
 
-    fastify.addHook('onRequest', function decorateRequestInstance (request, reply, next) {
-        request.bugsnag = Bugsnag;
+  fastify.addHook('onRequest', function decorateRequestInstance (request, reply, next) {
+    request.bugsnag = Bugsnag
 
-        next();
-    });
+    next()
+  })
 
-    fastify.addHook('onError', function bugsnagErrorHandlerPlugin (request, reply, error, next) {
-        if (options.enableReporting) {
-            this.bugsnag.notify(error, event => {
-                const { metadata, request: requestData } = obtainRequestAndMetadataInfo(request);
-                event.request = requestData;
+  fastify.addHook('onError', function bugsnagErrorHandlerPlugin (request, reply, error, next) {
+    if (options.enableReporting) {
+      this.bugsnag.notify(error, event => {
+        const { metadata, request: requestData } = obtainRequestAndMetadataInfo(request)
+        event.request = requestData
 
-                event.addMetadata('request', metadata);
-            });
-        }
+        event.addMetadata('request', metadata)
+      })
+    }
 
-        next();
-    });
+    next()
+  })
 
-    done();
+  done()
 }
 
 module.exports = fp(bugsnagPlugin, {
-    name: 'fastify-bugsnag',
-    fastify: '>=3.x',
-});
+  name: 'fastify-bugsnag',
+  fastify: '>=3.x'
+})
