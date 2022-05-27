86d48eb089687ecdd6b6695b122ad5aeec9d94a9
ZigaStrgar <ziga.strgar@gmail.com>
2022-05-27 12:08:32 +0200

    fix: tests for TS
    Signed-off-by: ZigaStrgar <ziga.strgar@gmail.com>

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,67 +1,67 @@
-import Fastify from 'fastify';
-import {test, mock} from 'tap';
+import Fastify from 'fastify'
+import { test, mock } from 'tap'
 
 import plugin from '../src/index'
-import {Event} from "@bugsnag/js";
+import { Event } from '@bugsnag/js'
 
 const apiKey = '00000000000000000000000000000000'
 
-test('Plugin decorates the instance and request', t => {
-    t.plan(4)
+await test('Plugin decorates the instance and request', async t => {
+  t.plan(4)
 
-    const fastify = Fastify()
-    fastify.register(plugin, {
-        apiKey
-    })
+  const fastify = Fastify()
+  await fastify.register(plugin, {
+    apiKey
+  })
 
-    fastify.ready(err => {
-        t.error(err)
+  fastify.ready(err => {
+    t.error(err)
 
-        t.ok(fastify.hasDecorator('bugsnag'))
-        t.ok(fastify.hasRequestDecorator('bugsnag'))
+    t.ok(fastify.hasDecorator('bugsnag'))
+    t.ok(fastify.hasRequestDecorator('bugsnag'))
 
-        t.type(fastify.bugsnag.notify, Function)
-    })
+    t.type(fastify.bugsnag.notify, Function)
+  })
 })
 
-test('Throw an error in handler and add request details to the error', async t => {
-    t.plan(1)
+await test('Throw an error in handler and add request details to the error', async t => {
+  t.plan(1)
 
-    const errors: Event[] = []
+  const errors: Event[] = []
 
-    const mockedPlugin = mock('../src/index', {
-        '@bugsnag/js': {
-            start: () => {
-            },
-            notify: (error: Event, event: Function) => {
-                errors.push(error)
-                event(error)
-            }
-        }
-    })
+  const mockedPlugin = mock('../src/index', {
+    '@bugsnag/js': {
+      start: () => {
+      },
+      notify: (error: Event, event: Function) => {
+        errors.push(error)
+        event(error)
+      }
+    }
+  })
 
-    const fastify = Fastify()
-    fastify.register(mockedPlugin, {
-        apiKey,
-        enableReporting: true
-    })
+  const fastify = Fastify()
+  await fastify.register(mockedPlugin, {
+    apiKey,
+    enableReporting: true
+  })
 
-    fastify.get('/error', () => {
-        throw new Error('Test')
-    })
+  fastify.get('/error', () => {
+    throw new Error('Test')
+  })
 
-    await fastify.inject({
-        url: '/error',
-        method: 'GET'
-    })
+  await fastify.inject({
+    url: '/error',
+    method: 'GET'
+  })
 
-    const [error] = errors
+  const [error] = errors
 
-    t.strictSame(error.request, {
-        clientIp: '127.0.0.1',
-        headers: {'user-agent': 'lightMyRequest', host: 'localhost:80'},
-        httpMethod: 'GET',
-        referer: undefined,
-        url: '/error'
-    })
+  t.strictSame(error.request, {
+    clientIp: '127.0.0.1',
+    headers: { 'user-agent': 'lightMyRequest', host: 'localhost:80' },
+    httpMethod: 'GET',
+    referer: undefined,
+    url: '/error'
+  })
 })
