29a8bea92ce33b57f94b854c0ffcc21bdad4bdc0
Žiga Strgar <ziga.strgar@gmail.com>
2022-12-07 20:17:49 +0100

    feat: Update tsconfig
    Signed-off-by: Žiga Strgar <ziga.strgar@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -58,6 +58,6 @@ function bugsnagPlugin (fastify: FastifyInstance, options: PluginOptions, done: 
 }
 
 export default fp(bugsnagPlugin, {
-  fastify: '^4.x',
+  fastify: '4.x',
   name: 'fastify-bugsnag'
 })
