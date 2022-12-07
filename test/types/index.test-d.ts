f7848f12cb9738fb6c797b188e90550526ef6302
Žiga Strgar <ZigaStrgar@users.noreply.github.com>
2022-12-07 20:24:34 +0100

    feat: Add tsd (#64)
    * feat: Add tsd for index
    
    Signed-off-by: Žiga Strgar <ziga.strgar@gmail.com>
    
    * fix: import alignments, test script
    
    Signed-off-by: Žiga Strgar <ziga.strgar@gmail.com>
    
    * fix: lint
    
    Signed-off-by: Žiga Strgar <ziga.strgar@gmail.com>
    
    Signed-off-by: Žiga Strgar <ziga.strgar@gmail.com>

---
+++ test/types/index.test-d.ts
@@ -0,0 +1,16 @@
+import { Client } from '@bugsnag/js'
+import fastify from 'fastify'
+import { expectType } from 'tsd'
+
+import plugin from '../../src/index'
+
+const app = fastify()
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+app.register(plugin).ready()
+
+expectType<Client>(app.bugsnag)
+
+app.get('/', request => {
+  expectType<Client>(request.bugsnag)
+})
