e010fe9a3fa4121ace8c5d05ee5a67ac1892732a
Žiga Strgar <ziga.strgar@gmail.com>
2023-07-13 21:45:37 +0200

    feat: test-d command
    Signed-off-by: Žiga Strgar <ziga.strgar@gmail.com>

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -2,7 +2,7 @@ import { Client } from '@bugsnag/js'
 import fastify from 'fastify'
 import { expectType } from 'tsd'
 
-import plugin from '../src'
+import plugin from '../../src'
 
 const app = fastify()
 
